3e111e742e0717510c924fae327d429835c5b2c2
MKrsna <krsna@localhost.localdomain>
2020-06-03 00:13:37 +0530

    Automatic Commit

--- event_registration/settings.py
+++ event_registration/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Application definition
 
---
+++ uploadToAws.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "Automatic Commit"
+git push -u origin master
+ssh -i "/atools/aws resources/ppkKey.pem" ubuntu@13.233.104.142 'cd /opt/eventBackend/EventRegistrationBackend/;git stash;git pull;pm2 restart 2;'
