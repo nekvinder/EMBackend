bfe5cbb2fb4cf0f1dcb0d7f0c526f839b84a6b50
root <root@localhost.localdomain>
2020-06-07 15:12:59 +0530

    Final

--- uploadToAws.sh
+++ uploadToAws.sh
@@ -4,3 +4,4 @@ git add .
 git commit -m "Automatic Commit"
 git push -u origin master
 ssh -i "/atools/aws resources/ppkKey.pem" ubuntu@13.233.104.142 'cd /opt/eventBackend/EventRegistrationBackend/;git stash;git pull;pm2 restart 2;'
+ssh -i "/atools/aws resources/ppkKey.pem" ubuntu@13.233.104.142 'cd /opt/eventBackend/EventRegistrationBackend/;'
