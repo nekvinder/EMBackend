6e5bc3bcff3696a78f8c6f5950797f34ce43422c
Nekvinder <nekvinder@gmail.com>
2020-08-17 13:49:06 +0530

    nekvinder.com added

--- deployToAWS.sh
+++ deployToAWS.sh
@@ -1,4 +1,4 @@
 cd ../
 zip -r b.zip EventRegistrationBackend/
-curl --insecure -v -k --key "/atools/aws_resources/ppkKey.pem" sftp://ubuntu@13.233.104.142/opt/Backends/b.zip -T "b.zip"
-ssh -i "/atools/aws_resources/ppkKey.pem" ubuntu@13.233.104.142 'cd /opt/Backends/;unzip -q -o b.zip;cd EventRegistrationBackend;pm2 start runWithSSL.json;'
+curl --insecure -v -k --key "/atools/aws_resources/ppkKey.pem" sftp://ubuntu@nekvinder.com/opt/Backends/b.zip -T "b.zip"
+ssh -i "/atools/aws_resources/ppkKey.pem" ubuntu@nekvinder.com 'cd /opt/Backends/;unzip -q -o b.zip;cd EventRegistrationBackend;pm2 start runWithSSL.json;'
--- event_registration/settings.py
+++ event_registration/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', 'django_filters',
-    'registration', 'corsheaders'
+    'registration', 'corsheaders','sslserver'
 ]
 
 MIDDLEWARE = [
